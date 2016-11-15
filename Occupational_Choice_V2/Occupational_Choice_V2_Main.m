4097277ed227c1ccc6b9f1afde4fd8bddb80537b
Sergio Ocampo Diaz <ocamp020@github.com>
2016-11-15 17:26:16 -0600

    add bisection

--- Occupational_Choice_V2/Occupational_Choice_V2_Main.m
+++ Occupational_Choice_V2/Occupational_Choice_V2_Main.m
@@ -20,6 +20,31 @@ diary('Log_Occupational_Choice.txt')
 %     options = optimset('Display','iter','TolFun',1e-05,'TolX',1e-06);
 %     [x,err,exitflag] = fminsearch(@(x) Find_DBN_Histogram(x),x_0,options);
     fprintf('Done! Time to compute: %2.2f seconds, error=%2.2d \n\n',toc(t0),err)
+    
+    % Bisection 
+    w_low  = 0.3 ; 
+    w_high = 0.5 ; 
+    excess_supply_low  = Find_DBN_Histogram(w_low)  ; 
+    excess_supply_high = Find_DBN_Histogram(w_high) ;
+    if excess_supply_low*excess_supply_high>0 
+        excess_supply = 1 ;
+        iter = 0 ;
+        while abs(excess_supply)>1e-4 && iter<maxIterations
+            w = (w_low+w_high)/2 ;
+            excess_supply = Find_DBN_Histogram(w)  ; 
+            if excess_supply>0 
+                w_high = w ; 
+            else 
+                w_low  = w ;
+            end 
+            iter = iter + 1 ;
+            disp([iter w_low w w_high excess_supply])
+        end 
+    else
+        disp('Equilibrium not bracketed')
+        return
+    end 
+    
 
     r_ben = r    ;
     w_ben = x(1) ; 
@@ -121,6 +146,8 @@ diary('Log_Occupational_Choice.txt')
 %     options = optimset('Display','iter','TolFun',1e-05,'TolX',1e-06);
 %     [x,err,exitflag] = fminsearch(@(x) Find_DBN_Histogram(x),x_0,options);
     fprintf('Done! Time to compute: %2.2f seconds, error=%2.2d \n\n',toc(t0),err)
+    
+    
 
     r_exp2 = r    ;
     w_exp2 = x(1) ; 
